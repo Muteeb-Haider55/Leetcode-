919cf6e4461759ac6cf718830707b36d35644939
Muteeb Haider <b-27240@student.usa.edu.pk>
2024-12-24 23:14:17 +0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (40.82%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+        vector<int> getConcatenation(vector<int> &nums)
+        {
+
+            vector<int> res(nums.size() *2);
+            int n = nums.size();
+            for (int i = 0; i < nums.size(); i++)
+            {
+                res[i] = nums[i];
+                res[i + n] = nums[i];
+            }
+            return res;
+        }
+};
