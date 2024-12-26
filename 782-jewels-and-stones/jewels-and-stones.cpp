db0694bda19b3bb621850fd4e87e882ceadc6b79
Muteeb Haider <b-27240@student.usa.edu.pk>
2024-12-26 18:46:04 +0500

    Time: 2 ms (18.86%) | Memory: 8.6 MB (37.23%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        int numJewelsInStones(string jewels, string stones) {
+              set <char> jewelset(jewels.begin(),jewels.end());
+                    int count=0;
+                          for(int i=0;i<stones.length();i++){
+
+                                      if (jewelset.count(stones[i])){
+                                                      count++;
+
+                                                              }
+                                                                    }
+                                                                    return count;
+                                                                        }
+                                                                        };
+
+
+        
