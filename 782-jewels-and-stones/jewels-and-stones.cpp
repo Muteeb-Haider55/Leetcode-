679c060c014f7815714c5a3d8f27952c8788e535
Muteeb Haider <b-27240@student.usa.edu.pk>
2024-12-29 19:47:03 +0500

    Time: 2 ms (18.91%) | Memory: 8.5 MB (39.55%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,18 +1,18 @@
-class Solution {
+class Solution
+{
     public:
-        int numJewelsInStones(string jewels, string stones) {
-              set <char> jewelset(jewels.begin(),jewels.end());
-                    int count=0;
-                          for(int i=0;i<stones.length();i++){
+        int numJewelsInStones(string jewels, string stones)
+        {
+            set<char> jewelset(jewels.begin(), jewels.end());
+            int count = 0;
+            for (int i = 0; i < stones.length(); i++)
+            {
 
-                                      if (jewelset.count(stones[i])){
-                                                      count++;
-
-                                                              }
-                                                                    }
-                                                                    return count;
-                                                                        }
-                                                                        };
-
-
-        
+                if (jewelset.count(stones[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+};
