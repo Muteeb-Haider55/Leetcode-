03303d70a0ce0b2dc34bca6a8a43261cd7d92d18
Muteeb Haider <b-27240@student.usa.edu.pk>
2025-01-14 21:54:35 +0500

    Time: 4 ms (29.04%) | Memory: 8.4 MB (53.36%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,22 +1,36 @@
 class Solution {
-    public:
-        string mergeAlternately(string word1, string word2) {
-                string merge;
-                       int i,j,k;
-                              i=j=k=0;
-                                     while(i<word1.length()&&j<word2.length()){
-                                             merge.push_back(word1[i++]);
-                                                     merge.push_back(word2[j++]);
-                                                            }
-                                                                   while(i<word1.length())
-                                                                          merge.push_back(word1[i++]);
-                                                                                 while(j<word2.length())
-                                                                                        merge.push_back(word2[j++]);
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0;
+        int j = 0;
+        string res = "";
+        int n = word1.size();
+        int m = word2.size();
+        while (i < n || j < m) {
+            if (i < n) {
+                res += word1[i];
+                i++;
+            }
+            if (j < m) {
+                res += word2[j];
+                j++;
+            }
+        }
+        return res;
+    }
+};
 
-                                                                                               return merge;
-                                                                                                   }
+// string merge;
+// int i, j, k;
+// i = j = k = 0;
+// while (i < word1.length() && j < word2.length())
+// {
+//     merge.push_back(word1[i++]);
+//     merge.push_back(word2[j++]);
+// }
+// while (i < word1.length())
+//     merge.push_back(word1[i++]);
+// while (j < word2.length())
+//     merge.push_back(word2[j++]);
 
-                                                                                                   };
-
- 
-  
+// return merge;
